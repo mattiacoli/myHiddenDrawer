af2de38ab1f41b4a95b052e92d7c142dbd9d319a
mattiacoli <mattia.coli@aol.com>
2025-05-06 15:50:26 +0200

    Add fontsource variables for Lexend Deca and Red Hat Text; update App component and styles

--- webapp_frontend/src/App.jsx
+++ webapp_frontend/src/App.jsx
@@ -5,7 +5,20 @@ function App() {
 
   return (
     <>
-      <h1>Hello</h1>
+      <div className="container p-4">
+        <h1>I nostri prodotti</h1>
+        <p>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Cumque voluptatem id ducimus culpa rerum adipisci velit accusamus praesentium aliquid, tempore, at ut iure eaque odio voluptatum similique rem animi error iste ab! Omnis quidem molestiae enim eius possimus, aut cupiditate eos provident quas nobis porro nisi, officiis quis? Eveniet, deleniti.
+        </p>
+
+
+        <h1>Ultimi arrivi</h1>
+
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit unde provident quam modi doloremque odio. Magnam, ad aliquam quos earum quod illo inventore aliquid dolores, cupiditate consequuntur optio velit fugiat.
+        </p>
+
+      </div>
 
     </>
   )
--- webapp_frontend/src/main.jsx
+++ webapp_frontend/src/main.jsx
@@ -1,6 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+
 import './index.css'
+// Supports weights 300-700
+import '@fontsource-variable/red-hat-text';
+// Supports weights 100-900
+import '@fontsource-variable/lexend-deca';
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
