9a7834408785e225418ff5540c7ae0961b587201
tommaso.dare <tommaso.dare96@gmail.com>
2025-05-07 16:21:01 +0200

    Add error handling middleware for 404 and 500 responses

--- webapp_backend_api/server.js
+++ webapp_backend_api/server.js
@@ -2,9 +2,20 @@ const express = require('express');
 const app = express();
 const port = 3000;
 const cors = require('cors');
+const errorHandler = require('./middlewares/serverError');
+const notFound = require('./middlewares/notFound');
 
+// Middleware
+app.use(cors(
+    {
+        origin: process.env.FRONT_URL || 'http://localhost:5173',
+    }
+))
+
+// Middleware per parsing JSON
 app.use(express.json());
 
+// Middleware per assets middleware
 app.use(express.static('public'));
 
 app.get('/', (req, res) => {
@@ -15,3 +26,6 @@ app.listen(port, () => {
     console.log(`Server is running at http://localhost:${port}`);
 })
 
+app.use(errorHandler)
+
+app.use(notFound)
