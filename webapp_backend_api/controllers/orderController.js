4f4462f3267a17f4133041894edc8fe8c8a3d33f
vincenzo-terracciano <vincenzo.terracciano97@gmail.com>
2025-05-08 09:59:51 +0200

    Refactor store function to improve input validation and error handling

--- webapp_backend_api/controllers/orderController.js
+++ webapp_backend_api/controllers/orderController.js
@@ -22,16 +22,25 @@ function show(req, res) {
 
 function store(req, res) {
     const { customer_id, order_number, price, shipping_price, total_price } = req.body;
+
+    // Validate input fields
+    if (!customer_id || !order_number || !price || !shipping_price || !total_price) {
+        return res.status(400).json({ err: "All fields are required" });
+    }
+
     const created_at = new Date().toISOString().slice(0, 19).replace('T', ' ');
     const updated_at = created_at;
     const sql = 'INSERT INTO orders (customer_id, order_number, price, shipping_price, total_price, created_at, updated_at) VALUES (?, ?, ?, ?, ?, ?, ?)';
     const values = [customer_id, order_number, price, shipping_price, total_price, created_at, updated_at];
 
     connection.query(sql, values, err => {
-        if (err) return res.status(500).json({ err: "Database query failed" });
+        if (err) {
+            console.error(err);
+            return res.status(500).json({ err: "Database query failed" })
+        }
 
-        res.status(201).json({ message: 'Order added successfully' })
-    })
+        res.status(201).json({ message: 'Order added successfully' });
+    });
 }
 
 module.exports = {
