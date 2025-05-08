3ad091122df28ab98bfa393cbd640b917d761b72
mattiacoli <mattia.coli@aol.com>
2025-05-08 10:52:12 +0200

    feat: add routing and layout components with header and footer

--- webapp_frontend/src/App.jsx
+++ webapp_frontend/src/App.jsx
@@ -1,24 +1,29 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import DefaultLayout from "./layouts/DefaultLayouts"
 
+//pages
+import Homepage from "./pages/Homepage"
 
 function App() {
 
 
   return (
     <>
-      <div className="container p-4">
-        <h1>I nostri prodotti</h1>
-        <p>
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Cumque voluptatem id ducimus culpa rerum adipisci velit accusamus praesentium aliquid, tempore, at ut iure eaque odio voluptatum similique rem animi error iste ab! Omnis quidem molestiae enim eius possimus, aut cupiditate eos provident quas nobis porro nisi, officiis quis? Eveniet, deleniti.
-        </p>
+      <BrowserRouter>
+        <Routes >
+          <Route Component={DefaultLayout}>
+
+            <Route path="/" Component={Homepage} />
 
 
-        <h1>Ultimi arrivi</h1>
 
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit unde provident quam modi doloremque odio. Magnam, ad aliquam quos earum quod illo inventore aliquid dolores, cupiditate consequuntur optio velit fugiat.
-        </p>
 
-      </div>
+
+
+          </Route>
+        </Routes>
+      </BrowserRouter>
+
 
     </>
   )
---
+++ webapp_frontend/src/layouts/DefaultLayouts.jsx
@@ -0,0 +1,23 @@
+import { Outlet } from 'react-router-dom'
+import Header from '../components/Header'
+import Footer from '../components/Footer'
+
+
+export default function DefaultLayout() {
+  return (
+    <>
+      <Header />
+
+      <main>
+        <Outlet />
+      </main>
+
+      <Footer />
+
+
+
+    </>
+
+
+  )
+}
