17f2c13402b4ef77e0a4c7e921af852cdeab0aa2
“alerenna” <alessandrorennafm@gmail.com>
2025-05-09 22:12:35 +0800

    feat: update product page with tags, categories, and enhanced details

--- webapp_frontend/src/pages/Product.jsx
+++ webapp_frontend/src/pages/Product.jsx
@@ -36,7 +36,7 @@ export default function Product() {
                                 {product.description}
                             </p>
                             <button className="btn btn-primary btn-lg" type="button">
-                                Acquista
+                                Aggiungi al carrello
                             </button>
                         </div>
                     </div>
@@ -47,6 +47,14 @@ export default function Product() {
 
             {/* Product */}
             <div className="container">
+                {/* Tags */}
+                <div className="p-2">
+                    {product.tags?.map((tag, index) => (
+                        <span key={index} className="badge bg-primary me-1 text-center">
+                            {tag.name}
+                        </span>
+                    ))}
+                </div>
                 <div className="row">
                     {/* Product images */}
                     {
@@ -58,10 +66,33 @@ export default function Product() {
 
                     }
                     {/* Product details & buy */}
-                    <div className="col-4">
-                        <h2>Prodotto</h2>
-                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius repudiandae ratione beatae dolore perferendis suscipit nemo, commodi, dignissimos odit dolorem incidunt illo laboriosam facere impedit, distinctio nulla corrupti totam cum.
-                        </p>
+                    <div className="col-4 sticky-details">
+                        <h4>
+                            {product.categories?.map((cat, index) => (
+                                <span key={index} className="badge bg-danger badge-categoria me-1">
+                                    {cat.name}
+                                </span>
+                            ))}
+                        </h4>
+                        <h2>{product.name}</h2>
+                        <p>{product.description}</p>
+                        <div className='fw-bold fs-3'>{product.price}€</div>
+                        <div>
+                            {product.available === 1 ? (
+                                <span>Prodotto disponibile <i className="bi bi-hand-thumbs-up-fill text-success"></i></span>
+                            ) : (
+                                <span>Non disponibile <i className="bi bi-hand-thumbs-down-fill text-danger"></i></span>
+                            )}
+                        </div>
+                        <ul className='list-unstyled mt-2'>
+                            <li><i class="bi bi-box-seam"></i> Pacco 100% anonimo</li>
+                            <li><i class="bi bi-check2-square"></i> Garanzia su prodotto di 2 anni</li>
+                            <li><i class="bi bi-tree"></i> Materiali certificati</li>
+                            <li><i class="bi bi-shield-lock"></i> Pagamento sicuro</li>
+                        </ul>
+                        <button className="btn btn-primary btn-lg mt-5" type="button">
+                            Aggiungi al carrello
+                        </button>
                     </div>
                 </div>
             </div>
