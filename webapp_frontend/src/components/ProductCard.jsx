7716995fae68971cd707e6670436423629231eeb
mattiacoli <mattia.coli@aol.com>
2025-05-09 15:39:44 +0200

    feat: add ProductCard component for displaying product details

---
+++ webapp_frontend/src/components/ProductCard.jsx
@@ -0,0 +1,19 @@
+export default function ProductCard({ item }) {
+  return (
+    <div key={item.id} className="col">
+
+      <div className="card h-100 shadow">
+        <div className="card-header bg-white ">
+          <img src={`${imageUrl}/${item.cover_image}`} alt="" className='card-img-top' />
+        </div>
+        <div className="card-body">
+          <h4>{item.name}</h4>
+          <p>
+            {item.description}
+          </p>
+          <p className='text-end fs-4'><strong>{item.price}</strong></p>
+        </div>
+      </div>
+    </div>
+  )
+}
