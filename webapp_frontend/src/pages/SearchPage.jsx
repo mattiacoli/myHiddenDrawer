366a9750e0af04e8663f791da161cbe23f437e20
mattiacoli <mattia.coli@aol.com>
2025-05-12 17:26:38 +0200

    fix: enhance search functionality by integrating category filtering in SearchPage

--- webapp_frontend/src/pages/SearchPage.jsx
+++ webapp_frontend/src/pages/SearchPage.jsx
@@ -6,73 +6,147 @@ import Searchbar from '../components/Searchbar'
 export default function SearchPage() {
   const [searchParams] = useSearchParams()
   const [filteredProducts, setFilteredProducts] = useState([])
-  const [category_query, set]
+  const [categoryQuery, setCategoryQuery] = useState([])
+  const [sortBy, setSortBy] = useState('')
+  const [isChecked, setIsChecked] = useState(false)
 
 
   const query = searchParams.get('q')
 
+  // sort function
+  /**
+   * Sorts an array of products based on the specified sort type.
+   *
+   * @param {Array<Object>} products - The array of product objects to be sorted.
+   * @param {string} sortType - The type of sorting to apply.
+   *   Possible values:
+   *   - 'price_asc': Sorts products by price in ascending order.
+   *   - 'price_desc': Sorts products by price in descending order.
+   *   - 'latest': Sorts products by creation date in descending order (newest first).
+   *   If no sortType is provided, the original array is returned unsorted.
+   * @returns {Array<Object>} - A new array of products sorted based on the specified sort type.
+   */
+  function sortProducts(products, sortType) {
+    if (!sortType) return products
+
+    const sortedProducts = [...products]
+    switch (sortType) {
+      case 'price_asc':
+        return sortedProducts.sort((a, b) => parseFloat(a.price) - parseFloat(b.price))
+      case 'price_desc':
+        return sortedProducts.sort((a, b) => parseFloat(b.price) - parseFloat(a.price))
+      case 'latest':
+        return sortedProducts.sort((a, b) => new Date(b.created_at) - new Date(a.created_at))
+      default:
+        return products
+    }
+  }
+
+  function handleCheck() {
+    setIsChecked(!isChecked)
+  }
+
+
+
   useEffect(() => {
-    if (query) {
+    if (query && categoryQuery) {
+      fetch(`http://localhost:3000/api/v1/products/search?name=${query}&category=${categoryQuery}`)
+        .then(res => res.json())
+        .then(data => {
+          let filteredData = isChecked ? (data.filter(product => product.promotion === 1)) : (data)
+          setFilteredProducts(sortProducts(filteredData, sortBy))
+        })
+        .catch(error => console.error('Error:', error))
+    } else if (query) {
       fetch(`http://localhost:3000/api/v1/products/search?name=${query}`)
         .then(res => res.json())
         .then(data => {
-          setFilteredProducts(data)
+          let filteredData = isChecked ? (data.filter(product => product.promotion === 1)) : (data)
+          setFilteredProducts(sortProducts(filteredData, sortBy))
         })
         .catch(error => console.error('Error:', error))
     }
+  }, [query, categoryQuery, sortBy, isChecked])
 
-    if (query && category_query) {
-      fetch(`http://localhost:3000/api/v1/products/search?name=${query}&category=${category_query}`)
-        .then(res => res.json())
-        .then(data => {
-          setFilteredProducts(data)
-        })
-        .catch(error => console.error('Error:', error))
-    }
+  return (
+    <>
+      <div className="p-5 mb-4 bg-light rounded-3 jumbotron">
+        <div className="container-fluid py-5 text-center">
+          <Searchbar />
 
+          {/* search filter */}
 
-  }
-  }, [query, category_query])
+          <div className="row row-cols-2">
+            <div className="col">
+              {/* category */}
+              <div className="my-3">
+                <label htmlFor="category" className="form-label">Categoria</label>
+                <select
+                  className="form-select form-select-lg"
+                  name="category"
+                  id="category"
+                  onChange={e => setCategoryQuery(e.target.value)}
+                >
+                  <option selected value=''>Tutti i prodotti</option>
+                  <option value="condom" >Condom</option>
+                  <option value="sex-toys">SexToys</option>
+                </select>
+              </div>
+            </div>
 
-return (
-  <>
-    <div className="p-5 mb-4 bg-light rounded-3 jumbotron">
-      <div className="container-fluid py-5 text-center">
-        <Searchbar />
+            {/* order by */}
+            <div className="col">
+              <div className="my-3">
+                <label htmlFor="sort" className="form-label">Ordina per</label>
+                <select
+                  className="form-select form-select-lg"
+                  name="sort"
+                  id="sort"
+                  onChange={e => setSortBy(e.target.value)}
+                >
+                  <option value="">Nessun ordinamento</option>
+                  <option value="price_asc">Prezzo crescente</option>
+                  <option value="price_desc">Prezzo decrescente</option>
+                  <option value="latest">Ultimi arrivi</option>
+                </select>
+              </div>
+            </div>
 
-        <div className="mb-3">
-          <label htmlFor="category" className="form-label">City</label>
-          <select
-            className="form-select form-select-lg"
-            name="category"
-            id="category"
-          >
-            <option selected>Select one</option>
-            <option value=""></option>
-            <option value="">Istanbul</option>
-            <option value="">Jakarta</option>
-          </select>
+            <div className="col">
+              <div class="form-check">
+                <label htmlFor="promo">In Sconto</label>
+                <input
+                  class="form-check-input"
+                  name="promo"
+                  id="promo"
+                  type="checkbox"
+                  value={isChecked}
+                  aria-label="Text for screen reader"
+                  onChange={handleCheck}
+                />
+              </div>
+
+            </div>
+          </div>
         </div>
-
-
-
-
       </div>
-    </div>
 
-    <div className="container">
-      {filteredProducts?.length > 0 ? (
-        <div className="row row-cols-sm-1 row-cols-md-2 row-cols-lg-4 gy-4">
-          {filteredProducts.map(item => (
-            <ProductCard item={item} key={item.id} />
-          ))}
-        </div>
-      ) : (
-        <div className='text-center' style={{ minHeight: '80vh' }}>
-          <h2>Nessun prodotto trovato</h2>
-        </div>
-      )}
-    </div>
-  </>
-)
+
+
+      {/* products list */}
+      <div className="container">
+        {filteredProducts?.length > 0 ? (
+          <div className="row row-cols-sm-1 row-cols-md-2 row-cols-lg-4 gy-4">
+            {filteredProducts.map(item => (
+              <ProductCard item={item} key={item.id} />
+            ))}
+          </div>
+        ) : (
+          <div className='text-center' style={{ minHeight: '80vh' }}>
+            <h2>Nessun prodotto trovato</h2>
+          </div>
+        )}
+      </div>
+    </>
+  )
 }
