de48ce97411286d4a1c8c51900c4170cb2572b01
tommaso.dare <tommaso.dare96@gmail.com>
2025-05-12 15:58:21 +0200

    Add RelatedProducts component to display related items with pricing and discounts

--- webapp_frontend/src/components/RelatedProducts.jsx
+++ webapp_frontend/src/components/RelatedProducts.jsx
@@ -1,10 +1,12 @@
 import { useState, useEffect } from 'react'
 import { useParams } from 'react-router-dom'
+import { Link } from 'react-router-dom'
 
 export default function RelatedProducts() {
 
     const [relatedProducts, setRelatedProducts] = useState([])
     const { slug } = useParams()
+    const imageUrl = `http://localhost:3000/images`
 
 
     useEffect(() => {
@@ -20,6 +22,68 @@ export default function RelatedProducts() {
     return (
 
         <>
+
+            <div className="container related-products">
+                <h2>Prodotti Correlati</h2>
+
+
+                <div className="row row-cols-1 row-cols-md-2 row-cols-lg-4 g-4">
+                    {
+                        relatedProducts.slice(0, 4).map(item => (
+                            <div key={item.id} className="col">
+                                <Link to={`/products/${slug}`} className='text-decoration-none' >
+
+                                    <div className={`card`} style={{ height: '500px' }}>
+                                        {
+                                            item?.discount_percentage != 0 ? (
+
+                                                <>
+                                                    <div className="card-header bg-white">
+                                                        <div className='discount badge text-bg-danger fs-5'>{parseFloat(item.discount_percentage).toFixed(0)}%</div>
+                                                        <img src={item.cover_image.length > 0 ? `${imageUrl}/${item.cover_image}` : `${imageUrl}/placeholder.jpg`} alt="" className='card-img-top' />
+                                                    </div>
+                                                    <div className="card-body d-flex flex-column justify-content-between">
+                                                        <h4>{item.name}</h4>
+                                                        <p className=' '>
+                                                            {item.description}
+                                                        </p>
+                                                        <div className="pricing text-end d-flex gap-3 justify-content-end  mt-auto">
+                                                            <p className='old_price fs-4'><s>{item.price} &#8364;</s></p>
+                                                            <p className='final_price fs-4 fw-bold'>{item.final_price} &#8364;</p>
+                                                        </div>
+                                                    </div>
+                                                </>
+                                            ) : (
+                                                <>
+                                                    <div className="card-header bg-white ">
+                                                        <img src={`${imageUrl}/${item.cover_image}`} alt="" className='card-img-top' />
+                                                    </div>
+                                                    <div className="card-body d-flex flex-column justify-content-between">
+                                                        <h4>{item.name}</h4>
+                                                        <p className=' '>
+                                                            {item.description}
+                                                        </p>
+                                                        <p className='text-end fs-4 mt-auto'><strong>{item.price} &#8364;</strong></p>
+                                                    </div>
+                                                </>
+                                            )
+                                        }
+                                    </div>
+
+                                </Link>
+
+                            </div>
+
+                        ))
+                    }
+                </div>
+
+            </div>
+
+
+
+
+
 
         </>
 
