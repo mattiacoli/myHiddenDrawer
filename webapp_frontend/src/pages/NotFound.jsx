abdb1eec7f6bb44f503656b04ed7b14ea081a73a
tommaso.dare <tommaso.dare96@gmail.com>
2025-05-13 16:29:06 +0200

    feat: update NotFound page with user-friendly message and styling

--- webapp_frontend/src/pages/NotFound.jsx
+++ webapp_frontend/src/pages/NotFound.jsx
@@ -4,9 +4,21 @@ export default function NotFound() {
 
     return (
 
-        <h1>Ciao ciao</h1>
+        <>
+
+            <div className="container text-center py-5">
+                <h2 className="display-4 mb-4">Pagina non trovata</h2>
+                <h3 className="mb-4">Oops... Sei finito in un punto sensibile! 😳</h3>
+                <p className="lead mb-4">
+                    La pagina che cerchi non esiste... ma abbiamo tanti modi per farti sentire meglio.
+                </p>
+                <Link to="/" className="btn btn-primary">
+                    Torna alla Homepage
+                </Link>
+            </div>
+
+        </>
 
     )
-
 
 }
