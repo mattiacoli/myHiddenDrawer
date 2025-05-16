83361a72b7c087f0ac2ac511c991a5c4e0415d59
mattiacoli <mattia.coli@aol.com>
2025-05-16 15:46:12 +0200

    fix: update CORS middleware to allow all origins in development feat: add allowedHosts configuration to Vite server for local development

--- webapp_frontend/vite.config.js
+++ webapp_frontend/vite.config.js
@@ -4,4 +4,13 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    allowedHosts: [
+      'localhost',
+      '127.0.0.1',
+      'ab69-51-179-98-199.ngrok-free.app',
+      // You can add a wildcard for all ngrok hosts if needed
+      // '*.ngrok-free.app'
+    ]
+  }
 })
